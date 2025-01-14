e9f0ed4ca99d5a8179e4446b6cb3954e82a0fcb6
Awkunss <tquockhanh2004@gmail.com>
2025-01-14 10:05:00 +0700

    Delete cache, output file

--- tracking/arguments.py
+++ tracking/arguments.py
@@ -6,12 +6,9 @@ def parse_args():
     Parse command-line arguments for the YOLOv9 tracking application.
     """
     parser = argparse.ArgumentParser(description="YOLOv9 Object Tracking")
+
     parser.add_argument(
-        "--input_type", type=int, choices=[1, 2], required=True,
-        help="Input type: 1 for video file, 2 for webcam."
-    )
-    parser.add_argument(
-        "--video_path", type=str, default=None,
-        help="Path to the video file (required if input_type is 1)."
+        "--input", type=str, default=None,
+        help="Path to the video file or '0' for webcam"
     )
     return parser.parse_args()
